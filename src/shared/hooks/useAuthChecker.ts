e67f0e53fb669a814194013e066e33950cf4207a
SUHONG\topic <topic828@Naver.com>
2024-10-31 17:46:54 +0900

    localstorage를 활용한 권한 확인

---
+++ src/shared/hooks/useAuthChecker.ts
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router';
+import { PROTECTED_ROUTES } from '../const/route';
+
+export const useAuthChecker = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isProtectedRoute = PROTECTED_ROUTES.includes(location.pathname);
+
+  useEffect(() => {
+    const auth = localStorage.getItem('auth');
+
+    if (isProtectedRoute && !auth) {
+      navigate('/auth');
+    }
+  }, [isProtectedRoute, navigate]);
+};
--- src/shared/layout/index.tsx
+++ src/shared/layout/index.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router';
+import { useAuthChecker } from '../hooks/useAuthChecker';
 
 const RootLayout = () => {
+  useAuthChecker();
+
   return (
     <div>
-      {' '}
       <Outlet />
     </div>
   );
